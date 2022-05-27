8ebe4267f3b593d8921a89c6d786b30ed24853cd
stianhsu <stianhsu@uio.no>
2022-05-27 15:09:26 +0200

    lagt til dropdownmeny

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,5 +1,18 @@
-<script setup lang="ts">
-    import router from '../router';
+<script lang="ts">
+        export default {
+        methods: {
+            visMobilMeny() {
+                const dropD = Array.from(document.getElementsByClassName('MobilValg') as HTMLCollectionOf<HTMLElement>)[0];
+                if (window.getComputedStyle(dropD).display === "none") {
+                    dropD.setAttribute("style", "visibility: visible");
+                    dropD.setAttribute("style", "display: inline");
+                } else {
+                    dropD.setAttribute("style", "visibility: invisible");
+                    dropD.setAttribute("style", "display: none");
+                }
+            },
+        },
+    };
 </script>
 <template>
     <div class="sticky top-0">
@@ -7,20 +20,30 @@
         <div class="BannerH">
           <div class="flex justify-between">
             <div>
-              <button @click="router.push({ name: 'Hjemskjerm' })"><h1 class="text-left font-mono text-4xl text-white py-2 hover:scale-105 transition ease-in-out">Stian Sundkvist</h1></button>
+              <button @click="$router.push({ name: 'Hjemskjerm' })"><h1 class="text-left font-mono text-4xl text-white py-2 hover:scale-105 transition ease-in-out">Stian Sundkvist</h1></button>
             </div>
             <div class="MenyKnapper">
               <ul class="flex space-x-6 text-white font-mono text-4xl py-2">
-                <li><button @click="router.push({ name: 'Prosjektskjerm' })" class="hover:underline hover:scale-105 transition ease-in-out">prosjekter</button></li>
-                <li><button @click="router.push({ name: 'Omskjerm' })" class="hover:underline hover:scale-110 transition ease-in-out">om</button></li>
-                <li><button @click="router.push({ name: 'CvSkjerm' })" class="hover:underline hover:scale-110 transition ease-in-out">cv</button></li>
-                <li><button @click="router.push({ name: 'Kontaktskjerm' })" class="hover:underline hover:scale-105 transition ease-in-out">kontakt</button></li>
+                <li><button @click="$router.push({ name: 'Prosjektskjerm' })" class="hover:underline hover:scale-105 transition ease-in-out">prosjekter</button></li>
+                <li><button @click="$router.push({ name: 'Omskjerm' })" class="hover:underline hover:scale-110 transition ease-in-out">om</button></li>
+                <li><button @click="$router.push({ name: 'CvSkjerm' })" class="hover:underline hover:scale-110 transition ease-in-out">cv</button></li>
+                <li><button @click="$router.push({ name: 'Kontaktskjerm' })" class="hover:underline hover:scale-105 transition ease-in-out">kontakt</button></li>
               </ul>
             </div>
             <div class="MobilMeny">
-                <button><h1>|||</h1></button>
+                <div>
+                    <button @click="visMobilMeny()"><h1 class="KnappTest">|||</h1></button>
+                </div>
             </div>
           </div>
+          <div class="MobilValg">
+              <ul class="grid grid-rows-4 text-white text-bold font-mono text-lg py-2">
+                <li><button @click="$router.push({ name: 'Prosjektskjerm' })" class="hover:underline hover:scale-105 transition ease-in-out">prosjekter</button></li>
+                <li><button @click="$router.push({ name: 'Omskjerm' })" class="hover:underline hover:scale-110 transition ease-in-out">om</button></li>
+                <li><button @click="$router.push({ name: 'CvSkjerm' })" class="hover:underline hover:scale-110 transition ease-in-out">cv</button></li>
+                <li><button @click="$router.push({ name: 'Kontaktskjerm' })" class="hover:underline hover:scale-105 transition ease-in-out">kontakt</button></li>
+              </ul>
+            </div>
         </div>
       </div>
     </div>
@@ -36,6 +59,22 @@
     max-w-6xl mx-auto px-3 py-8
 }
 
+.MobilValg {
+    @apply
+    hidden
+    bg-gradient-to-b
+    from-emerald-400
+    to-hoved
+    shadow-md
+    rounded-3xl
+    float-right
+    py-32
+    w-3/4
+    text-center
+
+
+}
+
 .MobilMeny {
     @apply
     text-3xl
@@ -44,6 +83,12 @@
     text-white
     mx-20 rotate-[90deg]
     hover:scale-110
+}
+
+.DropDown {
+    @apply
+    grid grid-rows-4
+
 }
 
 @media only screen and (max-width: 900px){
